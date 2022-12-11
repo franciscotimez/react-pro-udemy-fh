1df602c655b1d93e89912885ebe30c2bd9f3b032
Francisco Timez <franciscotimez@gmail.com>
2022-12-11 19:24:10 -0300

    Rename chunks

--- 02-react-app/src/routes/routes.ts
+++ 02-react-app/src/routes/routes.ts
@@ -8,9 +8,9 @@ interface Route {
   name: string;
 }
 
-const Lazy1 = React.lazy(() => import('../01-lazyload/pages/LazyPage1'));
-const Lazy2 = React.lazy(() => import('../01-lazyload/pages/LazyPage2'));
-const Lazy3 = React.lazy(() => import('../01-lazyload/pages/LazyPage3'));
+const Lazy1 = React.lazy(() => import(/* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1'));
+const Lazy2 = React.lazy(() => import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2'));
+const Lazy3 = React.lazy(() => import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3'));
 
 export const routes: Route[] = [
   {
