779e54ec285a37ead5adb87f0f40bb2dd2b952e0
Francisco Timez <franciscotimez@gmail.com>
2022-12-11 18:34:52 -0300

    Base completa

--- 02-react-app/src/routes/Navigation.tsx
+++ 02-react-app/src/routes/Navigation.tsx
@@ -1,14 +1,32 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom";
 
-import logo from '../logo.svg'
+import logo from '../logo.svg';
 export const Navigation = () => {
   return (
     <BrowserRouter>
-    <div className="main-layout">
-      <nav>
-        <img src={logo} alt="react-logo"/>
-      </nav>
-    </div>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="react-logo" />
+          <ul>
+            <li>
+              <NavLink to="/home" className={({ isActive }) => isActive ? 'nav-active' : ''}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="about" element={<h1>About Page</h1>} />
+          <Route path="users" element={<h1>Users Page</h1>} />
+          <Route path="home" element={<h1>Home Page</h1>} />
+
+          <Route path="/*" element={<Navigate to="home" replace />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  )
-}
+  );
+};
