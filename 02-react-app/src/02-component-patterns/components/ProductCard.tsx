564f9669caf363037aa1878124e65c634db742c3
Francisco Timez <franciscotimez@gmail.com>
2022-12-12 17:45:33 -0300

    Compound Component Pattern first step

--- 02-react-app/src/02-component-patterns/components/ProductCard.tsx
+++ 02-react-app/src/02-component-patterns/components/ProductCard.tsx
@@ -13,20 +13,42 @@ interface Product {
   img?: string;
 }
 
+export const ProductImage = ({ img = '' }) => {
+  return (
+    <img className={styles.productImg} src={img ? img : noImage} alt="Product" />
+  );
+};
+
+export const ProductTitle = ({ title }: { title: string; }) => {
+  return (
+    <span className={styles.productDescription}>{title}</span>
+  );
+};
+
+interface ProductButtonsProps {
+  counter: number,
+  increaseBy: (value: number) => void,
+}
+
+export const ProductButtons = ({ counter, increaseBy }: ProductButtonsProps) => {
+  return (
+    <div className={styles.buttonsContainer}>
+      <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>-</button>
+      <div className={styles.countLabel}>{counter}</div>
+      <button className={styles.buttonAdd} onClick={() => increaseBy(1)}>+</button>
+    </div>
+  );
+};
+
 export const ProductCard = ({ product }: Props) => {
 
   const { counter, increaseBy } = useProduct();
 
   return (
     <div className={styles.productCard}>
-      {/* <img className={styles.productImg} src="./coffee-mug.png" alt="Coffee Mug" /> */}
-      <img className={styles.productImg} src={product.img ? product.img : noImage} alt="Coffee Mug" />
-      <span className={styles.productDescription}>{product.title}</span>
-      <div className={styles.buttonsContainer}>
-        <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>-</button>
-        <div className={styles.countLabel}>{counter}</div>
-        <button className={styles.buttonAdd} onClick={() => increaseBy(1)}>+</button>
-      </div>
+      <ProductImage img={product.img} />
+      <ProductTitle title={product.title} />
+      <ProductButtons counter={counter} increaseBy={increaseBy} />
     </div>
   );
 };
