194c950ff65565f5f46be6081c9e2a8a64ce9e03
Francisco Timez <franciscotimez@gmail.com>
2022-12-12 15:40:51 -0300

    Receive props in the component

--- 02-react-app/src/02-component-patterns/components/ProductCard.tsx
+++ 02-react-app/src/02-component-patterns/components/ProductCard.tsx
@@ -3,14 +3,25 @@ import { useProduct } from '../hooks/useProduct';
 import styles from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
 
-export const ProductCard = () => {
+interface Props {
+  product: Product;
+}
+
+interface Product {
+  id: string;
+  title: string;
+  img?: string;
+}
+
+export const ProductCard = ({ product }: Props) => {
 
   const { counter, increaseBy } = useProduct();
+
   return (
     <div className={styles.productCard}>
-      <img className={styles.productImg} src="./coffee-mug.png" alt="Coffee Mug" />
-      {/* <img className={styles.productImg} src={noImage} alt="Coffee Mug" /> */}
-      <span className={styles.productDescription}>Coffee Mug</span>
+      {/* <img className={styles.productImg} src="./coffee-mug.png" alt="Coffee Mug" /> */}
+      <img className={styles.productImg} src={product.img ? product.img : noImage} alt="Coffee Mug" />
+      <span className={styles.productDescription}>{product.title}</span>
       <div className={styles.buttonsContainer}>
         <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>-</button>
         <div className={styles.countLabel}>{counter}</div>
--- 02-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 02-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,23 @@
 import { ProductCard } from "../components/ProductCard";
 
+const product = {
+  id: "1",
+  title: "Coffe Mug",
+  img: './coffee-mug.png'
+};
+
 export const ShoppingPage = () => {
   return (
     <div>
       <h1>Shopping</h1>
       <br />
-      <ProductCard />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+      }}>
+        <ProductCard product={product} />
+      </div>
     </div>
   );
 };
