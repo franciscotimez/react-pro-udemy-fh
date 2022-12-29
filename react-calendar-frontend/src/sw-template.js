015a4e637140cdb3d207a3482ac86d4b04221981
Francisco Timez <franciscotimez@gmail.com>
2022-12-29 11:45:47 -0300

    refactor

--- react-calendar-frontend/src/sw-template.js
+++ react-calendar-frontend/src/sw-template.js
@@ -8,27 +8,42 @@ const { registerRoute } = workbox.routing;
 const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
 const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
+const networkFirst = [
+  '/api/auth/renew',
+  '/api/events'
+];
 
 registerRoute(
-  new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
-  new CacheFirst()
-);
-
-
-registerRoute(
-  new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
-  new CacheFirst()
-);
-
-registerRoute(
-  new RegExp('http://localhost:4000/api/auth/renew'),
+  ({ request, url }) => {
+    if (networkFirst.includes(url.pathname)) return true;
+    return false;
+  },
   new NetworkFirst()
 );
 
+// registerRoute(
+//   new RegExp('http://localhost:4000/api/events'),
+//   new NetworkFirst()
+// );
+
+const cacheFirst = [
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
+];
+
 registerRoute(
-  new RegExp('http://localhost:4000/api/events'),
-  new NetworkFirst()
+  ({ request, url }) => {
+    if (cacheFirst.includes(url.href)) return true;
+    return false;
+  },
+  new CacheFirst()
 );
+
+// registerRoute(
+//   new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
+//   new CacheFirst()
+// );
+
 
 // Posteos Offline
 const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
