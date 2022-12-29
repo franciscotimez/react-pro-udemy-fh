3272dbbf5d2a09eece5f37c9fe2dcc548fde9b32
Francisco Timez <franciscotimez@gmail.com>
2022-12-29 11:26:49 -0300

    fin de sw

--- react-calendar-frontend/src/sw-template.js
+++ react-calendar-frontend/src/sw-template.js
@@ -1,3 +1,60 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
-workbox.precaching.precacheAndRoute( self.__WB_MANIFEST )
+workbox.loadModule('workbox-background-sync');
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
+
+
+registerRoute(
+  new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
+  new CacheFirst()
+);
+
+
+registerRoute(
+  new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
+  new CacheFirst()
+);
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/auth/renew'),
+  new NetworkFirst()
+);
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkFirst()
+);
+
+// Posteos Offline
+const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
+  maxRetentionTime: 24 * 60 // (minutes)
+});
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  'POST'
+);
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  'PUT'
+);
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  'DELETE'
+);
