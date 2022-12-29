bd6e6ef46816a8c5d647ca4a8ad62aae08450790
Francisco Timez <franciscotimez@gmail.com>
2022-12-29 11:54:25 -0300

    Detect Offline

--- react-calendar-frontend/src/components/ui/Navbar.js
+++ react-calendar-frontend/src/components/ui/Navbar.js
@@ -1,30 +1,39 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React from 'react';
+import { Offline, Online } from 'react-detect-offline';
+import { useSelector, useDispatch } from 'react-redux';
 import { startLogout } from '../../actions/auth';
 
 export const Navbar = () => {
 
     const dispatch = useDispatch();
-    const { name } = useSelector( state => state.auth );
+    const { name } = useSelector(state => state.auth);
 
     const handleLogout = () => {
-        dispatch( startLogout() );
-    }
+        dispatch(startLogout());
+    };
 
     return (
         <div className="navbar navbar-dark bg-dark mb-4">
             <span className="navbar-brand">
-                { name }
+                {name}
             </span>
-            
-            <button 
+
+            <Online>
+                <span className="text-success">Online</span>
+            </Online>
+
+            <Offline>
+                <span className="text-danger">Offline - Peticiones Guardadas en Cache</span>
+            </Offline>
+
+            <button
                 className="btn btn-outline-danger"
-                onClick={ handleLogout }
+                onClick={handleLogout}
             >
                 <i className="fas fa-sign-out-alt"></i>
                 <span> Salir</span>
             </button>
 
         </div>
-    )
-}
+    );
+};
